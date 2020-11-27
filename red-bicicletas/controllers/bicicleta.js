0f7dcaa499f5721178f2a69f2a83b4cd7328948a
yamil esquivel <niisanyamil@gmail.com>
2020-11-27 20:19:39 -0300

    update create

--- red-bicicletas/controllers/bicicleta.js
+++ red-bicicletas/controllers/bicicleta.js
@@ -5,3 +5,16 @@ exports.bicicleta_list = function(req,res){
 res.render('bicicletas/index', {bicis: Bicicleta.allBicis});
 
 }
+
+exports.bicicleta__create_get = function(req,res){
+	res.render('bicicletas/create',{});
+}
+exports.bicicleta__create_post = function(req,res){
+	var bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo);
+
+	bici.ubicacion = [req.body.lat, req.body.lng];
+	
+	Bicicleta.add(bici);
+
+	res.redirect('/bicicletas');
+}
